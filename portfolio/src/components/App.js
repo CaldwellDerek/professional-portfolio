00b3d6ffdd000b7637f4b8bd90886940d2f9bd4c
Derek Caldwell <djohncaldwell@gmail.com>
2023-06-01 17:35:18 -0700

    incorporated bootstrap, navbar added

--- portfolio/src/components/App.js
+++ portfolio/src/components/App.js
@@ -1,10 +1,13 @@
-
+import Navigation from '../components/navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <header>
+          <Navigation/>
+      </header>
     </div>
   );
 }
---
+++ portfolio/src/components/navbar/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const navStyle = {
+    height: "75px"
+}
+
+const Navigation = () => {
+    return (
+        <div style={navStyle}>
+            <Navbar className="h-100" bg="light" variant="light" >
+                <Navbar.Brand className="ms-2 w-100 d-flex justify-content-between" href="#about">Derek Caldwell</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#about">About</Nav.Link>
+                    <Nav.Link href="#projects">Projects</Nav.Link>
+                    <Nav.Link href="#contact">Contact</Nav.Link>
+                    <Nav.Link href="#resume">Resume</Nav.Link>
+                    <Nav.Link href="#github">GitHub</Nav.Link>
+                    <Nav.Link href="#linkedin">LinkedIn</Nav.Link>
+                </Nav>
+            </Navbar>
+        </div>
+    );
+}
+
+export default Navigation;
