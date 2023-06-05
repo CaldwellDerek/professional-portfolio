f2886e25e9c92f20132acedd95312b178f1a8d6f
Derek Caldwell <djohncaldwell@gmail.com>
2023-06-05 13:15:31 -0700

    corrected navbar links

--- portfolio/src/components/navigation/index.js
+++ portfolio/src/components/navigation/index.js
@@ -6,16 +6,17 @@ const navStyle = {
     height: "75px"
 }
 
+
 const Navigation = () => {
     return (
         <div style={navStyle}>
             <Navbar className="h-100" bg="dark" variant="dark" >
                 <Navbar.Brand className="ms-2 w-100 d-flex justify-content-between" href="#about">Derek Caldwell</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link to="/about">About</Nav.Link>
-                    <Nav.Link href="#projects">Projects</Nav.Link>
-                    <Nav.Link href="#contact">Contact</Nav.Link>
-                    <Nav.Link href="#contact">Resume</Nav.Link>
+                    <Nav.Link href="about">About</Nav.Link>
+                    <Nav.Link href="projects">Projects</Nav.Link>
+                    <Nav.Link href="contact">Contact</Nav.Link>
+                    <Nav.Link href="contact">Resume</Nav.Link>
                 </Nav>
             </Navbar>
         </div>
