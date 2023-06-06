9872d10be275024162f2a3395bca48dbcd085a5f
Derek Caldwell <djohncaldwell@gmail.com>
2023-06-06 15:33:12 -0700

    improved styling in navbar

--- portfolio/src/components/navigation/index.js
+++ portfolio/src/components/navigation/index.js
@@ -6,9 +6,9 @@ import './index.css';
 const Navigation = () => {
     return (
         <div className="nav-div">
-            <Navbar className="h-100" bg="dark" variant="dark" >
-                <Navbar.Brand className="ms-2 w-100 d-flex justify-content-between" href="#about">Derek Caldwell</Navbar.Brand>
-                <Nav className="me-auto">
+            <Navbar className="nav-bar" bg="dark" variant="dark" >
+                <Navbar.Brand className="nav-brand" href="#about">Derek Caldwell</Navbar.Brand>
+                <Nav className="nav-links ">
                     <Nav.Link href="about">About</Nav.Link>
                     <Nav.Link href="projects">Projects</Nav.Link>
                     <Nav.Link href="contact">Contact</Nav.Link>
