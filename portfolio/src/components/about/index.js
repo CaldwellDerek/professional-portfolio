d8abaf1e922276a4d431c7975baea2496e8c8e67
Derek Caldwell <djohncaldwell@gmail.com>
2023-06-06 15:17:34 -0700

    added button to about page

--- portfolio/src/components/about/index.js
+++ portfolio/src/components/about/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import './index.css'
 import profilePicture from "../../assets/profile-picture.jpg"
 
-
+const buttonClick = () => {
+    window.location.href = "/projects";
+}
 
 const About = () => {
     return (
@@ -11,13 +13,14 @@ const About = () => {
                     <img src={profilePicture} alt="self portrait" />
             </div>
             <div id="intro">
-                <h3>Hello, my name is</h3>
+                <p>Hello, my name is</p>
                 <h1>Derek Caldwell</h1>
                 <p>I am a full stack developer based out of the greater Seattle area with a passion for crafting user-centric web experiences and staying at the forefront of web development technologies.</p>
                 <p>My journey in computer science began in 2018 when I enrolled at Western Governor's University in their Software Development program. After graduating in 2022 with a Bachelor's of Science, I sought to expand my skillset and enroll at the University of Washington in a full stack web development bootcamp where I graduated in March of 2023. </p>
                 <p>
-                My time with both Universities has taught me much, and has given me the opportunity to develop full stack applications both independently and collabaratively. As a recent graduate, I'm eager to apply my skillsets and prove myself a valuable member in a development team.
+                My time with both Universities has taught me much, and has given me the opportunity to develop full stack applications both independently and collabaratively. As a recent graduate, I'm eager to apply my skillsets and prove myself as a valuable member in a development team.
                 </p>
+                <button type="button" onClick={buttonClick}>View my work!</button>
             </div>
         </div>
     );
