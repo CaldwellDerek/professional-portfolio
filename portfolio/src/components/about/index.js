d52b06fa0e6fbb7c30db6cc6cf092c6273343364
Derek Caldwell <djohncaldwell@gmail.com>
2023-06-06 11:48:31 -0700

    about portrait and opening text added

--- portfolio/src/components/about/index.js
+++ portfolio/src/components/about/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import profilePicture from "../../assets/profile-picture.jpg"
+
+const profileStyle = {
+    height: "200px",
+    width: "200px"
+}
 
 const About = () => {
     return (
         <div id="about">
-            <p>about test</p>
+            <img style={profileStyle} src={profilePicture} alt="self portrait" />
+            <h1>Hello.</h1>
+            <p>My name is Derek Caldwell, I am a full stack developer based out of the greater Seattle area with a passion for crafting user-centric web experiences and staying at the forefront of web development technologies.</p>
         </div>
     );
 }
