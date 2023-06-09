cf78b94d3070e8db7ad755d8689eca122821a932
Derek Caldwell <djohncaldwell@gmail.com>
2023-06-09 09:57:37 -0700

    improved spacing on projects page

--- portfolio/src/components/projects/index.js
+++ portfolio/src/components/projects/index.js
@@ -10,8 +10,8 @@ const Projects = () => {
                     <img className="project-image" src={placeholder} alt="placeholder" />
                 </div>
                 <div className="project-info">
-                    <h1>Project Name</h1>
-                    <p>Project description text.</p>
+                    <h1>Board Game Party</h1>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."</p>
                     <ul className="technologies-list">
                         <li className="technology">Language used</li>
                         <li className="technology">Language used</li>
@@ -32,7 +32,7 @@ const Projects = () => {
                 </div>
                 <div className="project-info">
                     <h1>Project Name</h1>
-                    <p>Project description text.</p>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."</p>
                     <ul className="technologies-list">
                         <li className="technology">Language used</li>
                         <li className="technology">Language used</li>
@@ -53,7 +53,7 @@ const Projects = () => {
                 </div>
                 <div className="project-info">
                     <h1>Project Name</h1>
-                    <p>Project description text.</p>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."</p>
                     <ul className="technologies-list">
                         <li className="technology">Language used</li>
                         <li className="technology">Language used</li>
