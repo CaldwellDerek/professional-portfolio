310c70281266e5bf6caa7a97abdccb75d999be27
Derek Caldwell <djohncaldwell@gmail.com>
2023-06-09 09:35:34 -0700

    first iteration of projects page

--- portfolio/src/components/projects/index.js
+++ portfolio/src/components/projects/index.js
@@ -1,9 +1,29 @@
 import React from "react";
+import placeholder from '../../assets/placeholder.jpg';
 import './index.css';
 const Projects = () => {
     return (
-        <div id="projects">
-            <p>projects test</p>
+        <div className="projects">
+            <div className="project-1">
+                <div className="project-image-container">
+                    <img className="project-image" src={placeholder} alt="placeholder" />
+                </div>
+                <div className="project-info">
+                    <h1>Project Name</h1>
+                    <p>Project description text.</p>
+                    <ul className="technologies-list">
+                        <li className="technology">Language used</li>
+                        <li className="technology">Language used</li>
+                        <li className="technology">Language used</li>
+                        <li className="technology">Language used</li>
+                        <li className="technology">Language used</li>
+                    </ul>
+                    <ul className="external-links-list">
+                        <li className="external-link">GitHub</li>
+                        <li className="external-link">Deployment</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
