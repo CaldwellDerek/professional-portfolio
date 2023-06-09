8db8129a4af8694367b146b7ac9e6fc69db96319
Derek Caldwell <djohncaldwell@gmail.com>
2023-06-09 10:05:57 -0700

    improved spacing and added dividing lines between projects

--- portfolio/src/components/projects/index.js
+++ portfolio/src/components/projects/index.js
@@ -26,10 +26,12 @@ const Projects = () => {
                 </div>
             </div>
 
+            <div className="divider"></div>
+
             <div className="project-2">
-                <div className="project-image-container">
+                {/* <div className="project-image-container">
                     <img className="project-image" src={placeholder} alt="placeholder" />
-                </div>
+                </div> */}
                 <div className="project-info">
                     <h1>Project Name</h1>
                     <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."</p>
@@ -45,7 +47,12 @@ const Projects = () => {
                         <li className="external-link">Deployment</li>
                     </ul>
                 </div>
+                <div className="project-image-container">
+                    <img className="project-image" src={placeholder} alt="placeholder" />
+                </div>
             </div>
+
+            <div className="divider"></div>
 
             <div className="project-3">
                 <div className="project-image-container">
