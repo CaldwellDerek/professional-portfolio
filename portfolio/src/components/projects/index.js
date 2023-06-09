09a90d7820156350424ed7f461e8a0e1b00bd98c
Derek Caldwell <djohncaldwell@gmail.com>
2023-06-09 09:42:16 -0700

    placeholders for 3 projects added, accounted for overflow

--- portfolio/src/components/projects/index.js
+++ portfolio/src/components/projects/index.js
@@ -4,7 +4,50 @@ import './index.css';
 const Projects = () => {
     return (
         <div className="projects">
+
             <div className="project-1">
+                <div className="project-image-container">
+                    <img className="project-image" src={placeholder} alt="placeholder" />
+                </div>
+                <div className="project-info">
+                    <h1>Project Name</h1>
+                    <p>Project description text.</p>
+                    <ul className="technologies-list">
+                        <li className="technology">Language used</li>
+                        <li className="technology">Language used</li>
+                        <li className="technology">Language used</li>
+                        <li className="technology">Language used</li>
+                        <li className="technology">Language used</li>
+                    </ul>
+                    <ul className="external-links-list">
+                        <li className="external-link">GitHub</li>
+                        <li className="external-link">Deployment</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="project-2">
+                <div className="project-image-container">
+                    <img className="project-image" src={placeholder} alt="placeholder" />
+                </div>
+                <div className="project-info">
+                    <h1>Project Name</h1>
+                    <p>Project description text.</p>
+                    <ul className="technologies-list">
+                        <li className="technology">Language used</li>
+                        <li className="technology">Language used</li>
+                        <li className="technology">Language used</li>
+                        <li className="technology">Language used</li>
+                        <li className="technology">Language used</li>
+                    </ul>
+                    <ul className="external-links-list">
+                        <li className="external-link">GitHub</li>
+                        <li className="external-link">Deployment</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="project-3">
                 <div className="project-image-container">
                     <img className="project-image" src={placeholder} alt="placeholder" />
                 </div>
