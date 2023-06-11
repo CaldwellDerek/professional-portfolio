86ca702ae1345d1d1115d08a70abc0185c9ffdc1
Derek Caldwell <djohncaldwell@gmail.com>
2023-06-11 16:15:29 -0700

    remove app files, introduced grid

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Navigation from './components/navigation';
+// import MobileNavigation from './components/mobile-navigation';
+import About from './components/about';
+import Projects from './components/projects';
+import Contact from './components/contact';
+import Footer from './components/footer';
 import './index.css';
-import App from './components/app/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Navigation/>
+    {/* <MobileNavigation/> */}
+    <About/>
+    <Projects/>
+    <Contact/>
+    <Footer/>
   </React.StrictMode>
 );
