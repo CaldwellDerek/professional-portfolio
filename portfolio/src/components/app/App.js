bac730718dacd340798eeab53ba58b394d12c24a
Derek Caldwell <djohncaldwell@gmail.com>
2023-06-10 17:40:36 -0700

    corrected about page overlapping projects on smaller screens

--- portfolio/src/components/app/App.js
+++ portfolio/src/components/app/App.js
@@ -9,9 +9,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Navigation/>
-      <MobileNavigation/>
       <div className='main'>
+        <Navigation/>
+        <MobileNavigation/>
         <About/>
         <Projects/>
       </div>
