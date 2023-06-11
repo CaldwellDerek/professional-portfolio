34dde9ebbc494133ebe9cc533721df4f773e9ee0
Derek Caldwell <djohncaldwell@gmail.com>
2023-06-11 09:04:23 -0700

    added mailto link for contact page

--- portfolio/src/components/contact/index.js
+++ portfolio/src/components/contact/index.js
@@ -6,7 +6,7 @@ const Contact = () => {
         <div id="contact">
             <h1>I'd love to hear from you</h1>
             <p>Reach out to me at my email below and I'll respond as soon as I'm able.</p>
-            <button type="button">djohncaldwell@gmail.com</button>
+            <a href="mailto:djohncaldwell@gmail.com"><button type="button">djohncaldwell@gmail.com</button></a>
         </div>
     );
 }
