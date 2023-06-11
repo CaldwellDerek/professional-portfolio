c03a8b11a0c037f29e8898f76ed3018355afc7eb
Derek Caldwell <djohncaldwell@gmail.com>
2023-06-11 16:56:38 -0700

    readjusting grid

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navigation from './components/navigation';
+// import Navigation from './components/navigation';
 // import MobileNavigation from './components/mobile-navigation';
 import About from './components/about';
-import Projects from './components/projects';
-import Contact from './components/contact';
-import Footer from './components/footer';
+// import Projects from './components/projects';
+// import Contact from './components/contact';
+// import Footer from './components/footer';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navigation/>
-    {/* <MobileNavigation/> */}
     <About/>
-    <Projects/>
-    <Contact/>
-    <Footer/>
   </React.StrictMode>
 );
